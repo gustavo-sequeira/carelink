f39ddbd1463233c5645c15a68a33788243a3a0f0
Gustavo Sequeira <sequeira2003@gmail.com>
2025-04-08 20:56:18 -0300

    iniciando projeto

---
+++ backend/src/main/java/br/com/carelink/CarelinkApplication.java
@@ -0,0 +1,13 @@
+package br.com.carelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarelinkApplication.class, args);
+	}
+
+}
