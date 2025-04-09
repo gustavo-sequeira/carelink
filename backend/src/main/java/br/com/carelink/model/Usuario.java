78eb1a938968cc82510188b6c52d43f14033925e
Gustavo Sequeira <sequeira2003@gmail.com>
2025-04-09 12:41:47 -0300

    iniciando o back com a classe de usuarios

---
+++ backend/src/main/java/br/com/carelink/model/Usuario.java
@@ -0,0 +1,18 @@
+package br.com.carelink.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+}
+
