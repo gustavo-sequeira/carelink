1e1548b87aa24aaddaca944788ab24c3d3f1453b
Gustavo Sequeira <sequeira2003@gmail.com>
2025-04-11 18:54:44 -0300

    ajuste na geracao do token

--- backend/src/main/java/br/com/carelink/security/SecurityConfig.java
+++ backend/src/main/java/br/com/carelink/security/SecurityConfig.java
@@ -1,6 +1,7 @@
-
 package br.com.carelink.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,14 +11,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @Configuration
 public class SecurityConfig {
 
-    private final JWTAuthFilter jwtAuthFilter;
     private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+    private final JWTAuthFilter jwtAuthFilter;
 
     public SecurityConfig(JWTAuthFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
@@ -30,25 +28,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-/*
-*                 .authorizeHttpRequests(auth -> auth
+        http.csrf(csrf -> csrf.disable())
+
+                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login/**","/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/api/usuarios/**").permitAll()
                         .anyRequest().authenticated()
                 )
-* */
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
-    private SecurityConfig() {
-        logger.info("Iniciando minhaFuncao com valor: {}", valor);
-        logger.debug("Debugando o valor: {}", valor);
-        logger.error("Exemplo de erro (apenas exemplo, sem exceção)");
-    }
 }
