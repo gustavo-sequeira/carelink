411bf1971cc462fc31da4019b23a146bd954fd1d
Gustavo Sequeira <sequeira2003@gmail.com>
2025-04-17 14:39:40 -0300

    ajustes para armazenar o token

--- frontend/src/componentes/BarraNavegacao/index.tsx
+++ frontend/src/componentes/BarraNavegacao/index.tsx
@@ -10,72 +10,92 @@ import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstr
 import './BarraNavegacao.css'
 import http from '../../http'
 
-interface PropsModalCandastro {
-  aberta: boolean
-  aoFechar: () => void
-  aoEfetuarLogin: () => void
-}
-
 const BarraNavegacao = () => {
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-  
-    const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      console.log('Login:', email, senha);
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
-      const usuario = {
-        email,
-        senha
+  const token = sessionStorage.getItem('token')
+  const [usuarioEstaLogado, setUsuarioEstaLogado] = useState<boolean>(token != null)
+
+  const aoEfetuarLogin = () => {
+    setUsuarioEstaLogado(true)
+  }
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Login:', email, senha);
+
+    const usuario = {
+      email,
+      senha
     }
 
-      http.post('auth/login', usuario)
+    http.post('auth/login', usuario)
       .then(resposta => {
-        
-         sessionStorage.setItem('token', resposta.data.access_token)
-         setEmail('')
-         setSenha('')
+
+        sessionStorage.setItem('token', resposta.data.access_token)
+        setEmail('')
+        setSenha('')
+        aoEfetuarLogin()
       })
       .catch(erro => {
         console.log(erro.response.data)
-          if(erro?.response?.data?.message){
-              alert(erro.response.data.message)
-          } else {
-              alert('Aconteceu algo inesperado ao efetuar o seu login')
-          }
-      })     
+        if (erro?.response?.data?.message) {
+          alert(erro.response.data.message)
+        } else {
+          alert('Aconteceu algo inesperado ao efetuar o seu login')
+        }
+      })
 
-    };
-  
-    return (
-      <Navbar className="navbar-custom" variant="dark" expand="lg" >
-        <Container fluid>
-          <div className='nomeempresa' >CareLink</div>
-          <Navbar.Collapse id="navbar-login">
+  };
 
-            <Form className="d-flex" onSubmit={handleLogin}>
-              <FormControl
-                type="email"
-                placeholder="Email"
-                className="me-2"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <FormControl
-                type="password"
-                placeholder="Senha"
-                className="me-2"
-                value={senha}
-                onChange={(e) => setSenha(e.target.value)}
-                required
-              />
-              <Button type="submit" variant="outline-success">Login</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+  return (
+    <Navbar className="navbar-custom" variant="dark" expand="lg" >
+      <Container fluid>
+        <div className='nomeempresa' >CareLink</div>
+
+
+        <ul className='acoes'>
+          {!usuarioEstaLogado && (<>
+            <Navbar.Collapse id="navbar-login">
+
+              <Form className="d-flex" onSubmit={handleLogin}>
+                <FormControl
+                  type="email"
+                  placeholder="Email"
+                  className="me-2"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <FormControl
+                  type="password"
+                  placeholder="Senha"
+                  className="me-2"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                  required
+                />
+                <Button type="submit" variant="outline-success">Login</Button>
+              </Form>
+            </Navbar.Collapse>
+          </>)}
+          {usuarioEstaLogado && <>
+            <BotaoNavegacao
+              texto=''
+              textoAltSrc=''
+              imagemSrc={notificacao}
+            />
+            <BotaoNavegacao
+              texto=''
+              textoAltSrc=''
+              imagemSrc={perfil}
+            />
+          </>}
+        </ul>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default BarraNavegacao
